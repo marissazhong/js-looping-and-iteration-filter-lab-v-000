d9d6b2e8b640a008179d789e925072c348952ece
Marissa Zhong <mwz2101@columbia.edu>
2018-07-16 00:14:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function findMatching(array, element) {
   return array.filter(function (item) {return item.toLowerCase() === element.toLowerCase()});
 }
 
-function fuzzyMatch() {
-  
+function fuzzyMatch(array, element) {
+  return array.filter(function (item) {return item.toLowerCase[0,element.length-1] === element})
 }
