5005cf796d71ba7385d9ad707a129dc501950cca
Marissa Zhong <mwz2101@columbia.edu>
2018-07-16 00:10:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 
 function findMatching(array, element) {
-  array.filter(item => item === element);
+  array.filter(function(item) {return item === element});
 }
